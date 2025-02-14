4505393ab394dabe77d4fe3bec5628757384dcbf
Amandinha254 <143639179+Amandinha254@users.noreply.github.com>
2025-02-14 00:01:33 -0300

    matriz de rotação e função de multiplicação de vetor e matriz

--- src/math.js
+++ src/math.js
@@ -30,7 +30,15 @@ function vec3normalize(vec)
 	return [vec[0]*isize, vec[1]*isize, vec[2]*isize];
 }
 
-
+function vec4MultplyMat4(vec,mat){
+	let vector = [
+		mat[ 0]*vec[0] + mat[ 1]*vec[1] + mat[ 2]*vec[2] + mat[ 3]*vec[3],
+		mat[ 4]*vec[0] + mat[ 5]*vec[1] + mat[ 6]*vec[2] + mat[ 7]*vec[3],
+		mat[ 8]*vec[0] + mat[ 9]*vec[1] + mat[10]*vec[2] + mat[11]*vec[3],
+		mat[12]*vec[0] + mat[13]*vec[1] + mat[14]*vec[2] + mat[15]*vec[3]
+	];
+	return vector;
+}
 
 
 /* matrix math */
@@ -45,6 +53,24 @@ function mat4identity()
 		0,0,0,1,
 	];
 };
+
+// returns rotation matrix 
+function mat4Rotation(angX=0, angY=0){
+	let rotX = [
+		1,         0,          0, 0,
+		0, cos(angX), -sin(angX), 0,
+		0, sin(angX),  cos(angX), 0,
+		0,         0,          0, 1
+	];
+	let rotY = [
+		cos(angY), 0, -sin(angY), 0,
+		        0, 1,          0, 0,
+		sin(angY), 0,  cos(angY), 0,
+		        0, 0,          0, 1
+	];
+	let mat = mat4Multiply(rotX,rotY);
+	return mat;
+}
 
 // returns the matrix for a specific transformation
 // the matrix is orthonormal
